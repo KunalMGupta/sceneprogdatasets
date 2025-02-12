14a57334946a0a23f9946e6ae4d6982f98be0af2
Kunal Gupta <kunalgupta@Kunals-Macbook.local>
2025-02-12 15:35:31 -0500

    trying to fix downloads

---
+++ sceneprogdatasets/download.sh
@@ -0,0 +1,5 @@
+cd future/assets/
+aws s3 cp s3://sceneprog-nautilus/sceneprogdatasets/future/ . --recursive
+cd ../../
+cd HSSD/assets/
+aws s3 cp s3://sceneprog-nautilus/sceneprogdatasets/hssd/ . --recursive
